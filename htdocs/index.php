15d493530bef3d5dfc2388489739a2b2732b9c26
Philippine SENTILHES <psentilh@e3r5p22.42.fr>
2019-06-10 19:20:32 +0200

    a bit more of html css

--- htdocs/index.php
+++ htdocs/index.php
@@ -5,13 +5,20 @@ session_start();
 <html>
 	<head>
 		<meta charset="utf-8"/>
+		<link href="https://fonts.googleapis.com/css?family=Ruslan+Display&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
 		<title>Camagru</title>
+		<link rel="icon" href="favicon.ico" />
 		<link rel="stylesheet" type="text/css" href="index.css">
 	</head>
 
+	<body>
 	<header>
 		<div id="head">
-			<a href="index.html"><h1>Camagru</h1></a>
+			<div id="title">
+				<a href="index.php"><h1>Camagru <span class="emoji">📺</span></h1></a>
+				<h3>Turn yourself into a TV star</h3>
+			</div>
 			<nav class="account">
 				<ul>
 					<li><a href="#">Sign-in</a></li>
@@ -21,11 +28,16 @@ session_start();
 			</nav>
 		</div>
 	</header>
-	<body>
-		<div id="page">
-			<div id="big_block">
-
-			</div>
+	<nav class="bar">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Montage</a></li>
+       	</ul>
+	</nav>
+	<div id="page">
+		<div id="big_block">
 		</div>
+	</div>
 	</body>
 </html>
